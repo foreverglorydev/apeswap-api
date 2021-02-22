1cebec1e926e296eea96393c5112946952bff582
ape-swap <ape.swap@hotmail.com>
2021-02-22 20:50:43 -0300

    fix: Test remove call to external api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ScheduleModule } from '@nestjs/schedule';
+// import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PairsModule } from './pairs/pairs.module';
