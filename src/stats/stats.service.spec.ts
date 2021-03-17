02061607d3e5de7aaa034d95ff89260d14042011
Raul Castro <casor06@gmail.com>
2021-03-16 21:58:00 -0600

    Add tests for the statistics service. - Test that the service is defined. - Test that the statistics data are as indicated. - Test that the wallet statistics data are as indicated. - Test error code in case of wrong wallet

--- src/stats/stats.service.spec.ts
+++ src/stats/stats.service.spec.ts
@@ -19,7 +19,54 @@ describe('StatsService', () => {
   });
 
   it('should get all stats', async () => {
+    const objStats = {
+      bananaPrice: expect.any(Number),
+      burntAmount: expect.any(Number),
+      totalSupply: expect.any(Number),
+      marketCap: expect.any(Number),
+      pools: expect.any(Array),
+      farms: expect.any(Array),
+      incentivizedPools: expect.any(Array),
+    };
     const stats = await service.getAllStats();
-    console.log(stats);
+    expect(stats).toEqual(expect.objectContaining(objStats));
+  });
+
+  it('should get stats for wallet', async () => {
+    const objWallet = {
+      tvl: expect.any(Number),
+      bananaPrice: expect.any(Number),
+      aggregateApr: expect.any(Number),
+      aggregateAprPerDay: expect.any(Number),
+      aggregateAprPerWeek: expect.any(Number),
+      aggregateAprPerMonth: expect.any(Number),
+      bananasEarnedPerDay: expect.any(Number),
+      bananasEarnedPerWeek: expect.any(Number),
+      bananasEarnedPerMonth: expect.any(Number),
+      bananasEarnedPerYear: expect.any(Number),
+      dollarsEarnedPerDay: expect.any(Number),
+      dollarsEarnedPerWeek: expect.any(Number),
+      dollarsEarnedPerMonth: expect.any(Number),
+      dollarsEarnedPerYear: expect.any(Number),
+      bananasInWallet: expect.any(Number),
+      pendingReward: expect.any(Number),
+      pools: expect.any(Array),
+      farms: expect.any(Array),
+      incentivizedPools: expect.any(Array),
+    };
+    const wallet = '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da6';
+    const statsWallet = await service.getStatsForWallet(wallet);
+    expect(statsWallet).toEqual(expect.objectContaining(objWallet));
+  });
+
+  it('should show invalid wallet address error code', async () => {
+    const objCodeError = {
+      code: expect.any(Number),
+      msg: expect.any(String),
+    };
+    const wallet = '0x0841BD0B734E4F5853f0dD8d7Ea041c241fb0Da';
+    const statsWallet = await service.getStatsForWallet(wallet);
+
+    expect(statsWallet).toEqual(expect.objectContaining(objCodeError));
   });
 });
