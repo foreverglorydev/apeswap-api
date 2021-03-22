7310b5b6969c0a71eb59a3b52f922145df03dec3
ape-swap <ape.swap@hotmail.com>
2021-03-21 23:07:19 -0300

    feat: Add basic in memory endpoint cache

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,16 +1,25 @@
-import { Controller, Get, Logger, Param, Query } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  Controller,
+  Get,
+  Logger,
+  Param,
+  UseInterceptors,
+} from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { SubgraphService } from './subgraph.service';
 
 @Controller('stats')
+@UseInterceptors(CacheInterceptor)
 export class StatsController {
-  //private readonly logger = new Logger(DrawingService.name);
+  private readonly logger = new Logger(StatsController.name);
   constructor(
     private statsService: StatsService,
     private subgraphService: SubgraphService,
   ) {}
   @Get()
   async getAllStats() {
+    this.logger.debug('Called GET /stats');
     const stats = await this.statsService.getAllStats();
     const tvlData = await this.subgraphService.getTVLData();
 
@@ -22,6 +31,7 @@ export class StatsController {
 
   @Get(':wallet')
   async getStatsForWallet(@Param('wallet') wallet: string) {
+    this.logger.debug('Called GET /stats/:wallet');
     return this.statsService.getStatsForWallet(wallet);
   }
 }
--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,10 +1,15 @@
-import { Module, HttpModule } from '@nestjs/common';
+import { CacheModule, Module, HttpModule } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { SubgraphService } from './subgraph.service';
 import { StatsController } from './stats.controller';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    CacheModule.register({
+      ttl: 15,
+    }),
+    HttpModule,
+  ],
   providers: [StatsService, SubgraphService],
   controllers: [StatsController],
 })
