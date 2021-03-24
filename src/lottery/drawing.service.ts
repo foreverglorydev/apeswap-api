8f49d71e7cd835159e5beb07216e42c8c04eabfc
ape-swap <ape.swap@hotmail.com>
2021-03-24 19:21:45 -0300

    chore: New lottery hours

--- src/lottery/drawing.service.ts
+++ src/lottery/drawing.service.ts
@@ -17,7 +17,7 @@ export class DrawingService {
     private drawModel: Model<DrawDocument>,
   ) {}
 
-  lotteryDrawHoursUtc = [18, 19, 20, 21, 22, 23, 24];
+  lotteryDrawHoursUtc = [1, 23];
   isDrawing = false;
   isReset = true;
   web3 = getWeb3();
