6f8d7955fd14b257048ff0ac0ac00b48fcad19c2
ape-swap <ape.swap@hotmail.com>
2021-03-24 14:35:25 -0300

    chore: More lottery draw times

--- src/lottery/drawing.service.ts
+++ src/lottery/drawing.service.ts
@@ -17,7 +17,7 @@ export class DrawingService {
     private drawModel: Model<DrawDocument>,
   ) {}
 
-  lotteryDrawHoursUtc = [18, 22];
+  lotteryDrawHoursUtc = [18, 19, 20, 21, 22, 23, 24];
   isDrawing = false;
   isReset = true;
   web3 = getWeb3();
@@ -92,7 +92,7 @@ export class DrawingService {
         await this.draw();
         return 'draw';
       }
-    } else if (drawed && latestDrawHours + 1 === currentHour) {
+    } else if (drawed && latestDrawHours === currentHour) {
       this.logger.log('Resetting');
       await this.reset();
       return 'reset';
