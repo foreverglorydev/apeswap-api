9dcfe7af39c225c0517d0d3301f3b2530631c960
ape-swap <ape.swap@hotmail.com>
2021-03-27 16:32:12 -0300

    chore: New lottery contract

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,7 +3,7 @@ export default () => ({
   chainId: process.env.CHAIN_ID || 97,
   97: {
     lottery: {
-      address: '0xF06557e0a4C0aBe314035cB196141Ca303Cb91D0',
+      address: '0x3DC3dc7F7c803367e81AEE194914634B734Ef00E',
       adminAddress: '0xb5e1Ec9861D7c1C99cB3d79dd602cC6122F0d7dc',
       adminKey: process.env.LOTTERY_ADMIN_KEY,
     },
--- src/lottery/drawing.service.ts
+++ src/lottery/drawing.service.ts
@@ -17,7 +17,7 @@ export class DrawingService {
     private drawModel: Model<DrawDocument>,
   ) {}
 
-  lotteryDrawHoursUtc = [1, 23];
+  lotteryDrawHoursUtc = [19, 20, 21, 22];
   isDrawing = false;
   isReset = true;
   web3 = getWeb3();
@@ -40,7 +40,7 @@ export class DrawingService {
       if (aDiff == bDiff) {
         return a > b ? a : b;
       } else {
-        return aDiff > bDiff ? a : b;
+        return bDiff < aDiff ? b : a;
       }
     });
   }
