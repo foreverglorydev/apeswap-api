d3a10d5671fa67894e9aec16e6ae0f50c415d2f1
ape-swap <ape.swap@hotmail.com>
2021-03-27 20:45:53 -0300

    chore: Add more lottery draw hours

--- src/lottery/drawing.service.ts
+++ src/lottery/drawing.service.ts
@@ -17,7 +17,7 @@ export class DrawingService {
     private drawModel: Model<DrawDocument>,
   ) {}
 
-  lotteryDrawHoursUtc = [19, 20, 21, 22];
+  lotteryDrawHoursUtc = [0, 1, 2, 3];
   isDrawing = false;
   isReset = true;
   web3 = getWeb3();
