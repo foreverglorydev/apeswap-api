9e846156eb760821ecd48d2f93181204ef59f2d4
ape-swap <ape.swap@hotmail.com>
2021-03-28 21:13:05 -0300

    fix: Update lottery ticket price & reset after 1 hour

--- src/lottery/utils/lottery.date.ts
+++ src/lottery/utils/lottery.date.ts
@@ -1,5 +1,5 @@
-export const firstLottery = new Date(Date.UTC(2021, 3, 30, 2, 0, 0, 0)); // Time of the first Offical Lottery; There are 3 Tests lotteries
-export const numberOfTestLotteries = 3;
+export const firstLottery = new Date(Date.UTC(2021, 2, 30, 2, 0, 0, 0)); // Time of the first Offical Lottery; There are 3 Tests lotteries
+export const numberOfTestLotteries = 2;
 const hour = 60 * 60 * 1000;
 export const generateLotteryDate = (issueIndex: number): Date => {
   const lotteryDate = new Date(firstLottery);
