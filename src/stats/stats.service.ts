e7788875451bf316a8e8339a5dac302e4f47bdb1
Bi Ko <terrandomination@gmail.com>
2021-04-12 16:14:42 -0700

    Add comment for Hotfix

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -220,7 +220,7 @@ export class StatsService {
 
     const poolInfos = await Promise.all(
       [...Array(poolCount).keys()]
-        .filter((x) => x != 23)
+        .filter((x) => x != 23) // Hotfix: skip for now due to non-standard ABI issue
         .map(async (x) => this.getPoolInfo(masterApeContract, x)),
     );
 
