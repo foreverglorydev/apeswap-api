cfc7322bc4e89b8312e84ed7f5d35a3dbeec036c
Ixm 759 <759ixm@gmail.com>
2021-06-23 21:45:40 -0500

    chore: Remove schemas trading

--- src/trading/trading.module.ts
+++ src/trading/trading.module.ts
@@ -1,12 +1,5 @@
 import { CacheModule, Module, HttpModule } from '@nestjs/common';
 import { TradingService } from '../trading/trading.service';
-import { TradingStats, TradingStatsSchema } from './schema/trading.schema';
-import { TradeSeason, TradeSeasonSchema } from './schema/trade-season.schema';
-import {
-  TradingTodayStats,
-  TradingTodayStatsSchema,
-} from './schema/trading-stats-today.schema';
-import { MongooseModule } from '@nestjs/mongoose';
 import { StatsModule } from 'src/stats/stats.module';
 import { TradingController } from './trading.controller';
 
@@ -17,11 +10,6 @@ import { TradingController } from './trading.controller';
     }),
     HttpModule,
     StatsModule,
-    MongooseModule.forFeature([
-      { name: TradingStats.name, schema: TradingStatsSchema },
-      { name: TradeSeason.name, schema: TradeSeasonSchema },
-      { name: TradingTodayStats.name, schema: TradingTodayStatsSchema },
-    ]),
   ],
   providers: [TradingService],
   controllers: [TradingController],
