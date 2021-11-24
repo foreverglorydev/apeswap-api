dbd8c88451c35da0dbb5344b6884f051e2c9c95e
Ixm 759 <759ixm@gmail.com>
2021-11-23 18:42:26 -0600

    chore: Validation staff role authorization

--- src/middleware/auth-strapi.ts
+++ src/middleware/auth-strapi.ts
@@ -12,13 +12,16 @@ export class AuthStrapiMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     const URL_STRAPI = process.env.APESWAP_STRAPI_URL;
     try {
-      await axios({
+      const user = await axios({
         method: 'get',
         url: `${URL_STRAPI}/users/me`,
         headers: {
           Authorization: `Bearer ${req.headers.auth}`,
         },
       });
+
+      const roles = user.data?.roles.find((r) => r.id === 1)
+      if(!roles) throw new HttpException('unauthorized', HttpStatus.UNAUTHORIZED);
       next();
     } catch (error) {
       throw new HttpException('unauthorized', HttpStatus.UNAUTHORIZED);
