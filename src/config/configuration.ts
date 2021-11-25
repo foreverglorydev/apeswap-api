49587df97ba17c88209c37cd84a1d90f981564bf
Ixm 759 <759ixm@gmail.com>
2021-11-24 18:43:37 -0600

    chore: Change Iazo exposer address

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -25,7 +25,7 @@ export default () => ({
       'https://data-seed-prebsc-2-s2.binance.org:8545',
       'https://data-seed-prebsc-2-s2.binance.org:8545',
     ],
-    iazoExposer: '0xEa9CCB01BEC64c324DCf3dcfADB8f7Af41Dc6846'
+    iazoExposer: '0x0D7055A2aDf6eF7Fc66767312a002C0Bdd60D539'
   },
   56: {
     lottery: {
@@ -60,6 +60,6 @@ export default () => ({
       'https://bsc-dataseed3.binance.org:443',
       'https://bsc-dataseed4.binance.org:443', */
     ],
-    iazoExposer: '0xEa9CCB01BEC64c324DCf3dcfADB8f7Af41Dc6846'
+    iazoExposer: '0x0D7055A2aDf6eF7Fc66767312a002C0Bdd60D539'
   },
 });
