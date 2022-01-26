3efb29946a978b2d630e769e3a4c57ca19e9614c
Ixm 759 <759ixm@gmail.com>
2022-01-25 18:34:57 -0600

    fix: token address info

--- src/bitquery/bitquery.controller.ts
+++ src/bitquery/bitquery.controller.ts
@@ -21,7 +21,7 @@ import { BitqueryService } from './bitquery.service';
     @Get('/')
     async getBitquery(): Promise<any> {
       return await this.bitqueryService.getTokenInformation(
-          '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a9s',
+          '0x603c7f932ED1fc6575303D8Fb018fDCBb0f39a95',
           '0x55d398326f99059ff775485246999027b3197955'
         );
     }
